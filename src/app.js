d69bf98ba169b4119f4c703e7d9030b3b9c35d3f
Abhishek Tripathi <abhitripathi2312@gmail.com>
2021-04-25 16:55:07 +0530

    database done

--- src/app.js
+++ src/app.js
@@ -1,30 +1,38 @@
 const express = require("express");
 const path = require("path");
-const app= express();
+const app = express();
 const hbs = require("hbs");
-
+const Order = require("./models/base");
 require("./db/conn");
 
 const port = process.env.PORT || 8000;
 
-const static_path=path.join(__dirname, "../public" );
-const template_path=path.join(__dirname, "../templates/views" );
-const partials_path=path.join(__dirname, "../templates/partials" );
-
-
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.static(static_path));
-app.set("view engine","hbs");
-app.set("views",template_path);
+app.set("view engine", "hbs");
+app.set("views", template_path);
 hbs.registerPartials(partials_path);
+app.use(express.json());
 
-app.get("/" , (req,res) => {
-    res.render("index")  
+app.get("/", (req, res) => {
+	res.render("index");
 });
 
+app.post("/submit", async (req, res) => {
+	const newOrder = new Order(req.body);
+	newOrder
+		.save()
+		.then(data => {
+			res.send(data);
+		})
+		.catch(e => {
+			res.status(400).send(e);
+		});
+});
 
-
-
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`);
-})
+app.listen(port, () => {
+	console.log(`server is running on ${port}`);
+});
--- src/models/base.js
+++ src/models/base.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 
-const empSchema= new mongoose.Schema({
-    Name : {
-        type:String,
-        required:true
-    },
-    email : {
-        type:String,
-        required:true,
-        unique:true
-    },
-    time : {
-        type:String,
-        required:true
-    },
-    order: {
-        type:[String],
-        required:true
-    }
+const empSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	time: {
+		type: String,
+		required: true,
+	},
+	order: {
+		type: [String],
+		required: true,
+	},
+	suggestion: {
+		type: String,
+		required: true,
+	},
+});
 
-})
+const Order = mongoose.model("order", empSchema);
+
+module.exports = Order;
